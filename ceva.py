949c92979bca10703b6d322fa3317e8df73ac432
Mara Olaru <olarumara20@stud.ase.ro>
2023-04-07 10:37:39 +0300

    finished ci/cd on local

--- ceva.py
+++ ceva.py
@@ -1,15 +1,42 @@
 import subprocess
 import sys
+import argparse
 
-type=sys.argv[1]
+parser=argparse.ArgumentParser()
+cmdType=sys.argv[1]
 
+if cmdType=='build':
+    parser.add_argument('build',type=str)
+    parser.add_argument('--dockerFilePath',type=str)
+    parser.add_argument('--imageName',type=str)
+    parser.add_argument('--imageTag',type=str)
+    args=parser.parse_args()
+    
+    command=subprocess.run(["docker",args.build,"-t","{imageName}:{imageTag}".format(imageName=args.imageName,imageTag=args.imageTag),args.dockerFilePath])
+    # print(command) for testing only
+elif cmdType=='push':
+    parser.add_argument('push',type=str)
+    parser.add_argument('--username',type=str)
+    parser.add_argument('--imageName',type=str)
+    parser.add_argument('--imageTag',type=str)
+    args=parser.parse_args()
 
-if type=='build':
-    list_ceva=subprocess.run(["docker","build","-t","flask-app2",sys.argv[2]])
-    print(list_ceva)
-elif type=='curl':
-    list_ceva=subprocess.run(["curl",sys.argv[2]])
-    print(list_ceva) 
-elif type=='deploy':
-    list_ceva=subprocess.run(["docker","container","run","-p","5000:5000",sys.argv[2]])
-    print(list_ceva)
+    command=subprocess.run(["docker","image","tag",args.imageName,"{username}/{imageName}:{imageTag}".format(username=args.username,imageName=args.imageName,imageTag=args.imageTag)])
+    # print(command)
+    command=subprocess.run(["docker","image","push","{username}/{imageName}:{imageTag}".format(username=args.username,imageName=args.imageName,imageTag=args.imageTag)])
+    # print(command)
+elif cmdType=='deploy':
+    #using the image deployed in the public registry
+    parser.add_argument('deploy',type=str)
+    parser.add_argument('--imageName',type=str)
+    parser.add_argument('--imageTag',type=str)
+    args=parser.parse_args()
+
+    command=subprocess.run(["docker","container","run","-p","5000:5000","-d","{imageName}:{imageTag}".format(imageName=args.imageName,imageTag=args.imageTag)])
+    print(command)
+elif cmdType=='test':
+    parser.add_argument('test',type=str)
+    parser.add_argument('--endpoint',type=str)
+    args=parser.parse_args()
+    command=subprocess.run(["curl","-v",args.endpoint])
+    print(command) 
