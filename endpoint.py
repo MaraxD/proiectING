6766d62fc9f4e832325679845701d1307d2e43cd
Mara Olaru <olarumara20@stud.ase.ro>
2023-04-07 19:45:23 +0300

    modiffs on the json data are persistent now

--- endpoint.py
+++ endpoint.py
@@ -1,18 +1,12 @@
 from flask import Flask
 from flask import request
 import requests
-import uuid
+from urllib.parse import urlparse, parse_qs
 import json
+import os.path
 
 app = Flask(__name__)
 
-url='https://ott-details.p.rapidapi.com/advancedsearch?start_year=2015&end_year=2020'
-headers = {
-    'X-RapidAPI-Key': "18158db10dmshfd301d948367abbp19c882jsn15f2b8d7fbc6",
-    'X-RapidAPI-Host': "ott-details.p.rapidapi.com"
-    }
-
-data=requests.get(url,headers=headers).json()
 
 class Film:
     def __init__(self, genre, imdbrating, released, synopsis, title, type):
@@ -27,12 +21,32 @@ class Film:
     
     def toJson(self):
         return json.dumps(self,default=lambda o: o.__dict__)
+    
 
+if not os.path.isfile('./filme.json'):
+    #data is fetched and the json file is created locally
+    url='https://ott-details.p.rapidapi.com/advancedsearch?start_year=2015&end_year=2020'
+    headers = {
+        'X-RapidAPI-Key': "18158db10dmshfd301d948367abbp19c882jsn15f2b8d7fbc6",
+        'X-RapidAPI-Host': "ott-details.p.rapidapi.com"
+        }
 
-lista_filme=[]
-for film in data['results']:
-    lista_filme.append(film)
+    data=requests.get(url,headers=headers).json()
+    lista_filme=[]
+    for film in data['results']:
+        lista_filme.append(film)
+    
+    with open('filme.json','w') as f:
+        json.dump(lista_filme,f)
 
+#not the smartest way to do this
+lista_filme=[] 
+with open('filme.json') as f:
+    data=json.load(f)
+    for i in data:
+        lista_filme.append(i)
+
+#--app routes--
 @app.route('/')
 def films():
     return data
@@ -40,6 +54,7 @@ def films():
 @app.route('/films', methods=["POST","GET","DELETE"]) # type: ignore
 def addFilm():
     if request.method=='GET':
+        #the data is fetched from the file
         return lista_filme
     elif request.method=='POST':
         newFilm=Film(request.get_json()["genre"],
@@ -51,16 +66,23 @@ def addFilm():
         #encode into JSON format
         newFilmJSONData=json.dumps(newFilm.toJson(),indent=4)
         
-
         #decode
         newFilmJSON=json.loads(newFilmJSONData)
         newFilmJSON2=json.loads(newFilmJSON)
         lista_filme.append(newFilmJSON2)
-
+        with open('filme.json','w') as f:
+            json.dump(lista_filme,f)
+            
         return 'film added'
+    elif request.method=='DELETE':
+        id=[x for x in filme if x.imdbid==request.get_json()] # type: ignore
+        for i in filme: # type: ignore
+            if i.imdbid==id:
+                filme.pop(i)  # type: ignore
+
     
 # @app.route('/films/{id}'.format(id=request.get_json()["imdbid"]), methods=["GET"]) #type: ignore
 # def getFilmById():
-#     return [x for x in lista_filme if x.imdbid==id]
+#     return [x for x in lista_filme if x.imdbid==request.get]
          
 
